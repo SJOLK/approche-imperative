42dbda4d4e4ad365dae00c1aa0685401650f2198
SJOLK <123592790+SJOLK@users.noreply.github.com>
2024-10-02 15:26:52 +0200

    feat: TP2

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,28 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String arg[]) {
+        byte a = 12;
+        short b = 15324;
+        int c = -153596;
+        long d = 16715699L;
+        float e = 1.7F;
+        double f = 1.6;
+        char g = 'a';
+        boolean h = false;
+        String i = "bonjour tout le monde. ";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+
+        String randomString = "Voici le résultat d’un calcul : \n1+5=6";
+        System.out.println(randomString);
+    }
+}
